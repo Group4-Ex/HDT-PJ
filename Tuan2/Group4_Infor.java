4430a582fdf9e019b1f17e3cb8ba27ea78e51fa2
Tanz <wz.ngoctan@gmail.com>
2024-09-24 20:08:15 +0700

    Setup

--- Tuan2/Group4_Infor.java
+++
@@ -1,9 +0,0 @@
-public class Group_Infor {
-    public static void main(String[] args) {
-        System.out.println("Group Information");
-        System.out.println("1. Lê Ngọc Tân");
-        System.out.println("2. Đoàn Xuân Song");
-        System.out.println("3. Trần Văn Phúc");
-        System.out.println("4. Trần Văn Minh");
-    }
-}
