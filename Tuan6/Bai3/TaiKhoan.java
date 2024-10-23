90453682606b430e0dd571905d9dc5d3d6984c63
Tanz <wz.ngoctan@gmail.com>
2024-10-23 12:05:14 +0700

    Bai3_fix

--- Tuan6/Bai3/TaiKhoan.java
+++ Tuan6/Bai3/TaiKhoan.java
@@ -1,43 +1,88 @@
+package NganHang;
 
 import java.util.Scanner;
 
 public class TaiKhoan {
+    Scanner sc = new Scanner(System.in);
     private String soTK;
     private String tenTK;
-    private long soDuTK = 0;
-
-    public TaiKhoan(String soTK, String tenTK) {
+    private double soDu;
+    private String mk;
+    public TaiKhoan(String soTK, String tenTK, String mk) {
         this.soTK = soTK;
         this.tenTK = tenTK;
+        this.mk = mk;
     }
-    public TaiKhoan(){};
 
-    public void guiTien(){
-        System.out.println("Nhap vao so tien can gui: ");
-        long soTien = new Scanner(System.in).nextInt();
-        soDuTK += soTien;
+    public String getSoTK() {
+        return soTK;
     }
-    public void rutTien(){
-        System.out.println("Nha so tien muon rut: ");
-        int soTien = new Scanner(System.in).nextInt();
-        if(soTien > soDuTK)
-            System.out.println("So tien khong du de rut.");
-        else {
-            System.out.println("Rut thanh cong");
-            soDuTK -= soTien;
-        }
+
+    public void setSoTK(String soTK) {
+        this.soTK = soTK;
     }
 
     public String getTenTK() {
         return tenTK;
     }
 
-    public double getSoDuTK() {
-        return soDuTK;
+    public void setTenTK(String tenTK) {
+        this.tenTK = tenTK;
+    }
+
+    public double getSoDu() {
+        return soDu;
+    }
+
+    public void setSoDu(double soDu) {
+        this.soDu = soDu;
+    }
+
+    public String getMk() {
+        return mk;
     }
 
     @Override
     public String toString() {
-        return "Chu tai Khoan: "+tenTK+" || So du hien tai: "+ soDuTK;
+        return "Chu TK: "+tenTK +"|| SoTK :" + soTK+" || So du hien tai: "+soDu;
+    }
+
+    public void rutTien(){
+        System.out.println("Nhap vao so tien muon rut");
+        int soTien = sc.nextInt();
+        sc.nextLine();
+        if(soTien > soDu)
+            System.out.println("Khong du tien de rut");
+        else
+        {
+            System.out.println("Rut tien thanh cong");
+            soDu -= soTien;
+        }
+    }
+    public void nopTien(){
+        System.out.println("Nhap vao so tien muon nop");
+        int soTien = new Scanner(System.in).nextInt();
+        soDu += soTien;
+        System.out.println("Nop tien thanh cong");
+    }
+
+    public void doiMatKhau(){
+        int count = 0;
+        do {
+            System.out.println("Nhap vao mat khau cu");
+            String mkCu = sc.nextLine();
+            if(this.mk.equals(mkCu)){
+                System.out.println("Nhap vao mat khau moi");
+                String mkMoi = sc.nextLine();
+                this.mk = mkMoi;
+                System.out.println("Doi mat khau thanh cong");
+                break;
+            }
+            else
+            {
+                System.out.println("Sai mat khau");
+                count++;
+            }
+        }while (count < 5);
     }
 }
---
+++ Tuan6/Bai3/Test.java
@@ -0,0 +1,7 @@
+package NganHang;
+public class Test {
+    public static void main(String[] args) {
+        DSNguoiDung ds1 = new DSNguoiDung();
+        ds1.luaChon();
+    }
+}
